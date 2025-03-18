c4f17c544ed98905ee0672c5413c6295e85dd614
Vijay Sahani <sahani0vijay@gmail.com>
2025-03-18 14:26:22 +0530

    Printing Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    // declaring a String
+    String string = ("Hello World");
+
+    System.out.println(string);
+  }
+}
